9f46540245a7a2039ef29acdb0b152e9119faca9
suman-kim <zzsdsdsd@gmail.com>
2022-11-09 19:48:48 +0900

    header,footer 구분, 로그인화면 UI 구현

--- src/router.js
+++ src/router.js
@@ -3,8 +3,16 @@ import express from 'express';
 const router = express.Router();
 
 
-router.get('/',(req,res) => {
-	res.render('index')
+router.get('/index',(req,res) => {
+	res.render('index');
+})
+
+router.get('/login',(req,res) => {
+	res.render('login');
+})
+
+router.get('/signup', (req,res) => {
+	res.render('signup');
 })
 
 
